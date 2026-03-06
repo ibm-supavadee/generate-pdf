a3cbd0553117b5805fe3c80509b4913837f79ecf
Budsakorn Khosagrid <Budsakorn.Khosagrid@ibm.com>
2026-03-06 15:35:32 +0700

    json

--- src/controllers/pdf.controller.ts
+++ src/controllers/pdf.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { PdfService } from "../services/pdf/pdf.service";
 import { PdfERequestData } from "../services/pdf/models/pdf-erequest-data.model";
+import { termAndConERequestExistingMock } from "../mocks/termAndConERequestExisting.mock";
 
 export const createERequestPdf = async (req: Request, res: Response) => {
   try {
     const data = req.body as PdfERequestData;
+    data.termsAndConditions = termAndConERequestExistingMock;
 
     const base64Pdf = await PdfService.generateERequestPdf(data);
 
