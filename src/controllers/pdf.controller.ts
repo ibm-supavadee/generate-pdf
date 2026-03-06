4d2c291282d5d0ed147bac03e35e6955e353c9a2
Budsakorn Khosagrid <Budsakorn.Khosagrid@ibm.com>
2026-03-06 15:29:18 +0700

    edit body

--- src/controllers/pdf.controller.ts
+++ src/controllers/pdf.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { PdfService } from "../services/pdf/pdf.service";
+import { PdfERequestData } from "../services/pdf/models/pdf-erequest-data.model";
 
 export const createPdf = async (req: Request, res: Response) => {
   try {
-    const data = req.body;
+    const data = req.body as PdfERequestData;
 
     const base64Pdf = await PdfService.generateERequestPdf(data);
 
